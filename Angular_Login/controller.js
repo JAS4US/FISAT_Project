abb3f299823939ee1d3dfde5b8e55a2532213b4e
JAS4US <39548739+JAS4US@users.noreply.github.com>
2018-07-24 00:52:27 -0400

    Add files via upload

---
+++ Angular_Login/controller.js
@@ -0,0 +1,28 @@
+var app=angular.module('mainApp',['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/',{
+		templateUrl:'tst_login.html'
+		})
+.when('/dashboard',{
+		templateUrl:'dashboard.html'
+})
+.otherwise({
+
+	redirectTo: '/''
+});
+
+});
+
+app.controller('loginCtrl',function($scope,$location){
+	$scope.submit=function(){
+		var uname=$scope.username;
+var password=$scope.password;
+if(uname=="admin"&& password=="admin"){
+	$location.path('/dashboard');
+}
+
+	
+};
+});
